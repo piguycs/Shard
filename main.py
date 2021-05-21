9973556d95dabaa62959b361827bfc9c2c4abfac
RocKing1001 <44892832+RocKing1001@users.noreply.github.com>
2021-05-21 11:26:38 +0200

    Adds to path

--- main.py
+++ main.py
@@ -1,22 +1,39 @@
 import sys
 import json
 
-
+class files:
+    INSTALLED_JSON = ''
 
 class colors:
     GREEN_SUCCESS = '\033[1;32;40m'
     RED_FAILURE = '\033[1;31;40m'
+    YELLOW_INFO = '\033[1;33;40m'
     RESET = '\033[0;0;0m'
 
 
+def versionManagement(package):
+    currVersion = (package['version'])
+
+    # TESTING STUFF
+    newVersion = (v for v in package['version'].split("."))
+    
+    print(colors.GREEN_SUCCESS + "[FOUND]" + colors.RESET +
+          " Installed version: {}".format(currVersion))
+    print(colors.YELLOW_INFO + "[UPDATE]" + colors.RESET +
+          " LATEST version: {}".format(newVersion))
+
+
+
+
 def doStuff(package):
-    with open('usrfiles/package_list.json') as f:
+    with open(files.INSTALLED_JSON) as f:
         data = json.load(f)
 
     if package in data['packages']:
-        print(colors.GREEN_SUCCESS + "[FOUND]" + colors.RESET + " Dependencies: {}".format(data['packages'][package]["depends"]))
+        versionManagement(data['packages'][package])
     else:
-        print(colors.RED_FAILURE + "[NOT FOUND]" + colors.RESET + " The package you searched for is not present")
+        print(colors.RED_FAILURE + "[NOT FOUND]" + colors.RESET +
+              " The package you searched for is not present")
     
 
 def main(args):
@@ -25,27 +42,41 @@ def main(args):
         print("No args given")
     else:
         for x in args:
-            if args.index(x) == 0:
-                print("hmmmmm sus")
+            if args.index(x) != 0:
 
-            # Argument for help    
-            elif x == '-h':
-                print(
-                    colors.GREEN_SUCCESS + "[ARG: {}]".format(x) + 
-                    colors.RESET + " Shard is an open source package manager for spark, which is an open source package installer")
+                # Argument for help    
+                if x == '-h':
+                    print(
+                        colors.GREEN_SUCCESS + "[ARG: {}]".format(x) + 
+                        colors.RESET + " Shard is an open source package manager for spark, which is an open source package installer")
 
-            elif x == '-v':
-                print(colors.GREEN_SUCCESS + "[ARG: {}]".format(x) +
-                      colors.RESET + " My creator was tatching hentai so he forgot to give me a version".format(x))
-            
-            elif x == '-dog':
-                print(colors.GREEN_SUCCESS + "[ARG: {}]".format(x) +
-                      colors.RESET + " Bow wow, time to kill.sh".format(x))
-            
-            else: doStuff(x)
+                elif x == '-v':
+                    print(colors.GREEN_SUCCESS + "[ARG: {}]".format(x) +
+                        colors.RESET + " My creator was tatching hentai so he forgot to give me a version".format(x))
+                
+                elif x == '-dog':
+                    print(colors.GREEN_SUCCESS + "[ARG: {}]".format(x) +
+                        colors.RESET + " Bow wow, time to kill.sh".format(x))
+                
+                else: doStuff(x)
 
 
 
 if __name__ == '__main__':
-    print("Args: ", sys.argv)
-    main(sys.argv)
+
+    print("SHARD BOOTING UP")
+    file_exists = False
+
+    try:
+        with open('usrfiles/package_list.json') as f:
+            files.INSTALLED_JSON = 'usrfiles/package_list.json'
+            file_exists = True
+    except IOError:
+        try:
+            with open('/usr/spark/installed.json') as f:
+                files.INSTALLED_JSON = '/usr/spark/installed.json'
+                file_exists = True
+        except IOError:
+            print("SPARK IS NOT INSTALLED")
+
+    main(sys.argv) if file_exists else None
--- makefile
+++ makefile
@@ -2,6 +2,8 @@ buildnrun: main.py
 	@pyinstaller --onefile main.py
 	clear
 	@./dist/main
+	@sudo ln -sf `pwd`/dist/main /usr/bin/shard
+	@chmod +x /usr/bin/shard
 
 
 run:
