ffbab861447a255d950dde8bd63c0e0ac854cbad
ThePiGuy <44892832+RocKing1001@users.noreply.github.com>
2021-06-08 11:32:11 +0200

    Imports spark in shard files
    working on a faster way to update packages while spark does the packaging
    
    perhaps preparing for a custom package system. but u did not hear this from me

--- sparkModule.py
+++ sparkModule.py
@@ -1,6 +1,40 @@
-from sys import path
-path.append('/usr/bin/')
+from requests import get
+from os import popen as run
+from json import load, dumps
 
-import spark
+url = "https://api.github.com/repos/HUSKI3/Spark/releases/latest"
+latestVer = get(url).json()["name"].split(' ')[2]
 
-spark.ezconfig()
+def checkFile(file):
+    return 1 if 'y' in run(f'if test -f {file}; then echo y; fi').read() else 0
+
+def parseVersion(version):
+    realVersion = ''
+    for x in version:
+        if x.isdigit():
+            realVersion = realVersion + x
+
+    return realVersion
+
+def getCurrVer():
+    with open("versions.spark") as f:
+        currVer = load(f)["current"]
+        return currVer
+
+def sparkVersions(check):
+    if not checkFile("versions.spark") and check:
+        run("touch versions.spark")
+    elif not check:
+        return 1
+
+
+if __name__ == "__main__":
+    sparkVersions(1)     # Creates a version file if not present
+    if parseVersion(latestVer) > parseVersion(getCurrVer()):
+        sparkurl = get(url).json()["assets"][0]["browser_download_url"]
+        run("wget -q -O spark {}".format(sparkurl))
+
+        with open("versions.spark", "w") as f:
+            f.write(dumps('{"current": "{}"}'.format(latestVer), indent=4, sort_keys=True))
+
+
