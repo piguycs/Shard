8688a44499e3d93c81df0d9ee326f47dcaa67edd
ThePiGuy <44892832+RocKing1001@users.noreply.github.com>
2021-06-28 15:34:07 +0200

    Update dimv2.py

--- dimv2.py
+++ dimv2.py
@@ -4,11 +4,13 @@ from json import load
 from requests import get
 import ezjson
 
+repos = []
 dims = ezjson.getfile("dimensions.json")
 
 for x in dims["repos"]:
     print(x[0] +": "+ x[1])
-
-repos = ["https://sparkofficial-1.ubuntulove2004.repl.co"]
-
+    repos.append(x[1])
+print(repos)
+for x in repos:
+    print(get(x+"/packs").json()) if x != "none" else None
 # print(get(repos[0]+"/packs").json())
