646117b4450a216d2cfeec23b60bd4ad60c5b0e8
ThePiGuy <44892832+RocKing1001@users.noreply.github.com>
2021-06-30 09:48:56 +0200

    dim v2

--- dimv2.py
+++ dimv2.py
@@ -2,11 +2,22 @@
 
 from requests import get
 from ezjson import setCfg as conf
+from output import out
 
 dims = conf("dimensions.json")
 
 repos = dims.getAll("repos", 1)
 
+packageList = []
 for x in repos:
-    print(get(x+"/packs").json() if x != "none" else "None")
-# print(get(repos[0]+"/packs").json())
+    if x != "none":
+        packages = get(x+"/packs").json()
+        for y in packages:
+            try:
+                # print(y["name"] + " " + (y["url"] if y["url"] != '' else "BITCH URL MISSING"))
+                packageList.append(y["name"])
+            except KeyError:
+                print(y["name"] + " --> ERR: URL TAG MISSING")
+
+if "pain" in packageList:
+    print(out("[FOUND]", 1) + out("pain in repo", 4))
